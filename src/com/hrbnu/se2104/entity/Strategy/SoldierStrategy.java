d8ab91c151d6f470cb30db0f7c4626d12eb3bd46
jaryarbn <grandline501@gmail.com>
2022-08-09 14:10:26 +0800

    Complete Soldier move strategy

--- src/com/hrbnu/se2104/entity/Strategy/SoldierStrategy.java
+++ src/com/hrbnu/se2104/entity/Strategy/SoldierStrategy.java
@@ -4,9 +4,103 @@ import com.hrbnu.se2104.entity.Piece.ChessPiece;
 
 import java.util.Set;
 
-public class SoldierStrategy implements MoveStrategy{
+public class SoldierStrategy implements MoveStrategy {
+    private int currentX;
+    private int currentY;
+    private char camp;
+    private boolean firstMove = true;
+
+    public SoldierStrategy(int currentX, int currentY, char camp) {
+        this.currentX = currentX;
+        this.currentY = currentY;
+        this.camp = camp;
+    }
+
+    public int getCurrentX() {
+        return currentX;
+    }
+
+    public void setCurrentX(int currentX) {
+        this.currentX = currentX;
+    }
+
+    public int getCurrentY() {
+        return currentY;
+    }
+
+    public void setCurrentY(int currentY) {
+        this.currentY = currentY;
+    }
+
+    public boolean isFirstMove() {
+        return firstMove;
+    }
+
+    public void setFirstMove(boolean firstMove) {
+        this.firstMove = firstMove;
+    }
+
     @Override
     public boolean move(int targetX, int targetY, Set<ChessPiece> chessPieces) {
+
+        //直线移动
+        if (currentY == targetY) {
+            if (isOverPiece(targetX, targetY, camp, chessPieces))
+                return false;
+            switch (camp) {
+                case 'B': {
+                    if (isFirstMove() && (targetX == currentX + 1 || currentX + 2 == targetX)) {
+                        setFirstMove(false);
+                        currentY = targetY;
+                        currentX = targetX;
+                        return true;
+                    } else if (!isFirstMove() && currentX + 1 == targetX) {
+                        currentY = targetY;
+                        currentX = targetX;
+                        return true;
+                    }
+                    break;
+                }
+
+                case 'W': {
+                    if (isFirstMove() && (targetX == currentX - 1 || targetX == currentX - 2)) {
+                        setFirstMove(false);
+                        currentY = targetY;
+                        currentX = targetX;
+                        return true;
+                    } else if (!isFirstMove() && currentX - 1 == targetX) {
+                        currentY = targetY;
+                        currentX = targetX;
+                        return true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        //吃子移动
+        for (ChessPiece e : chessPieces) {
+            if (Math.abs(e.getRow() - currentY) == 1) {
+                if (e.getColumn() - currentX == 1 && e.getCamp() == 'W' ||
+                        currentX - e.getColumn() == 1 && e.getCamp() == 'B') {
+                    currentY = targetY;
+                    currentX = targetX;
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+
+    }
+
+    public boolean isOverPiece(int targetX, int targetY, char camp, Set<ChessPiece> chessPieces) {
+        for (ChessPiece piece : chessPieces) {
+            if (piece.getColumn() == targetX && piece.getRow() == targetY && !piece.isSelected()) {
+                return true;
+            }
+        }
         return false;
     }
 }
